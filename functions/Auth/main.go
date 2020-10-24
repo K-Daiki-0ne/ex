71d071708d2f129ef4ad08d603938cd88d17a4b1
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-24 23:31:33 +0900

    modified: auth server using Gin & fix makefile

--- functions/Auth/main.go
+++ functions/Auth/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	app := gin.Default()
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	fmt.Println("Auth server listening ...OK")
+	app.Run(":4000")
 }
