e826f39c53f18a32b944514cce0607df6c825274
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-25 17:06:58 +0900

    add: connect controller to router

--- functions/Auth/src/router/router.go
+++ functions/Auth/src/router/router.go
@@ -1,9 +1,13 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"EX/auth/src/controllers"
+
+	"github.com/gin-gonic/gin"
+)
 
 // Router : Auth server routering func
 func Router(app *gin.Engine) {
 	// Get login information
-	app.GET("/auth/login")
+	app.GET("/auth/login", controllers.Login)
 }
