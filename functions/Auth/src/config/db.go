729519571d9c16871f4a3cd3d642fb8ea66792d3
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-28 23:45:53 +0900

    create: base database fucntion

---
+++ functions/Auth/src/config/db.go
@@ -0,0 +1,16 @@
+package config
+
+// Connect : Database connect function
+func Connect() {
+	/*
+		Databaseに接続する処理を記述する。
+		使用するDBはMySQL
+	*/
+}
+
+// Close : Database close function
+func Close() {
+	/*
+		Databaseを切断する処理を記述する。
+	*/
+}
