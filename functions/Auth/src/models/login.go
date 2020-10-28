0b78ce7b1bb71e6d7418d578d092204deea490bd
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-28 23:34:51 +0900

    create: login model

---
+++ functions/Auth/src/models/login.go
@@ -0,0 +1,13 @@
+package models
+
+// Login Login model
+func Login(name string, password string) (string, string) {
+	/*
+	 　controllerで利用する
+	 　ユーザーネームとパスワードを引数で受け取り、データベースにアクセスする
+	 　取得したデータを返す。
+	*/
+	aaa := "dafsa"
+	pass := "fasfdsaf"
+	return aaa, pass
+}
