a15cec618ef79575b351e19780f0ccfea60c7615
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-28 22:12:10 +0900

    create: test login token

--- functions/Auth/src/controllers/login.go
+++ functions/Auth/src/controllers/login.go
@@ -1,13 +1,38 @@
 package controllers
 
 import (
+	"EX/auth/src/middleware"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+// User : post user type
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var user = User{
+	Username: "username",
+	Password: "password",
+}
+
 // Login : get user information from database
 func Login(c *gin.Context) {
-	response := "Login"
-	c.JSON(http.StatusOK, response)
+	var u User
+	err := c.ShouldBindJSON(&u)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+
+	if user.Username != u.Username || user.Password != u.Password {
+		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+		return
+	}
+
+	token, err := middleware.AuthMiddleware(1)
+
+	c.JSON(http.StatusOK, token)
 }
