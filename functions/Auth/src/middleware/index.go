b190b71f7ab3a23bd94ae832995fe95788eed010
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-28 21:52:33 +0900

    create: create token middleware

---
+++ functions/Auth/src/middleware/index.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// User type
+type User struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var user = User{
+	ID:       1,
+	Username: "username",
+	Password: "password",
+}
+
+// AuthMiddleware : JWT Middleware
+func AuthMiddleware(userid uint64) (string, error) {
+	var err error
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userid
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
