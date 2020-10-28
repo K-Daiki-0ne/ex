251582dec5e996499b68e8e41561ab072a498364
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-28 23:25:20 +0900

    add: comment

--- functions/Auth/src/controllers/login.go
+++ functions/Auth/src/controllers/login.go
@@ -23,11 +23,19 @@ func Login(c *gin.Context) {
 	name := c.Param("name")
 	pass := c.Param("pass")
 
+	/*
+	 * 取得したユーザーネームとパスワードと一致するかを確認する
+	 * ModelのLoginにnameとpassを引数に入れる
+	 */
+
+	// Databaseに存在するユーザーネームとパスワードがパラメーターのユーザーネームとパスワードと一致するかを判定する
+	// 一致しない場合はvalidate errorをレスポンスする。
 	if user.Username != name || user.Password != pass {
 		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
 		return
 	}
 
+	// MySQLのUserIDをAuthMiddlewareの引数に入れる
 	token, err := middleware.AuthMiddleware(1)
 
 	if err != nil {
