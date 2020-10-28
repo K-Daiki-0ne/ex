f936f1c24537399650d2c1bcf807b08fe3837358
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-28 23:38:50 +0900

    create: Register model

---
+++ functions/Auth/src/models/register.go
@@ -0,0 +1,13 @@
+package models
+
+// Register : register user information for database
+func Register(name string, pass string) string {
+	/*
+		取得したユーザー情報をデータベースに保存する。
+		データベースへの保存が成功した場合は成功
+		失敗した場合は失敗
+		とわかるような戻り値にする。
+	*/
+	response := "Succesfully"
+	return response
+}
