5bdfd646a1a0b5ec7e0515fa326d123eaef301ea
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-01 10:43:21 +0900

    modified: using godotenv

--- functions/Auth/src/middleware/index.go
+++ functions/Auth/src/middleware/index.go
@@ -1,17 +1,23 @@
 package middleware
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
 )
 
 // AuthMiddleware : JWT Middleware
 func AuthMiddleware(userid uint64) (string, error) {
 	var err error
-	// key := os.Getenv("SECRET_KEY")
-	os.Setenv("ACCESS_KEY", "key")
+	fatal := godotenv.Load()
+	if fatal != nil {
+		fmt.Println("Fatal Load .env")
+	}
+	key := os.Getenv("SECRET_KEY")
+	os.Setenv("ACCESS_KEY", key)
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
 	atClaims["user_id"] = userid
