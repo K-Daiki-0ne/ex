b4ca69ce46248f234d649b9eeb12722860d606f3
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-01 10:56:33 +0900

    create: parse db path

--- functions/Auth/src/config/path.go
+++ functions/Auth/src/config/path.go
@@ -1,9 +1,7 @@
 package config
 
 // Path : Database connect path
-func Path() {
-	/*
-		Databaseに接続するPathを製成する。
-	*/
-
+func Path(DBUser string, DBPass string, DBProt string, DBName string) string {
+	connect := DBUser + ":" + DBPass + "@" + DBProt + "/" + DBName + "?charset=utf8&parseTime=True&loc=Local"
+	return connect
 }
