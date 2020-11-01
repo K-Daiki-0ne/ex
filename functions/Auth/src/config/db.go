d05fc00cf7b57ef939e667631a2eb46cb6e0b2fa
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-01 10:54:46 +0900

    create: use db config

--- functions/Auth/src/config/db.go
+++ functions/Auth/src/config/db.go
@@ -1,5 +1,24 @@
 package config
 
+import "os"
+
+var (
+	// Dialect Use DataBase name
+	Dialect = os.Getenv("DB")
+
+	// DBUser MySQL's user name
+	DBUser = os.Getenv("DB_USER")
+
+	// DBPass MySQL's user password
+	DBPass = os.Getenv("DB_PASS")
+
+	// DBProt MySQL protocol
+	DBProt = os.Getenv("DB_PROT")
+
+	// DBName MySQL table name
+	DBName = os.Getenv("DB_NAME")
+)
+
 // Connect : Database connect function
 func Connect() {
 	/*
