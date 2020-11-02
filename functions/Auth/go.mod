281d7ac3ecad18a713ca071787859465c7a651cd
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-02 23:44:02 +0900

    modified: database connect object

--- functions/Auth/go.mod
+++ functions/Auth/go.mod
@@ -20,4 +20,5 @@ require (
 	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gorm.io/driver/mysql v1.0.3
+	gorm.io/gorm v1.20.4
 )
--- functions/Auth/src/database/database.go
+++ functions/Auth/src/database/database.go
@@ -1,10 +1,15 @@
 package database
 
-/*
-	Databaseに接続できる変数
-*/
+import (
+	"EX/auth/src/config"
+
+	"github.com/jinzhu/gorm"
+
+	// MySQL Driver
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 var (
 	// DBConnect config/db.go instance object
-	DBConnect string
+	DBConnect *gorm.DB = config.Connect()
 )
