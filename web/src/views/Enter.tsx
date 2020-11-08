e28826b871630076e482f4c15794469b54c8af7f
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-08 22:48:50 +0900

    delete: comment

--- web/src/views/Enter.tsx
+++ web/src/views/Enter.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/View.module.css';
 const Enter: React.FC = (): JSX.Element => {
   return (
     <div className={styles.view}>
-      {/* <img src="pub/EX.jpg" alt="EX" width={400} height={200} /> */}
+      
     </div>
   )
 }
