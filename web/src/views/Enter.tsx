ca8261706817e9adf322a95772ace77f352ba28c
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-08 22:13:45 +0900

    create: base enter page

---
+++ web/src/views/Enter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Image from 'next/image'
+import styles from '../styles/View.module.css';
+
+const Enter: React.FC = (): JSX.Element => {
+  return (
+    <div className={styles.view}>
+      {/* <img src="pub/EX.jpg" alt="EX" width={400} height={200} /> */}
+    </div>
+  )
+}
+
+export default Enter
