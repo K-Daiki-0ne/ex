ee745d8a7f2d7cdfc854d4db0b560e5fc787a49b
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-08 22:54:11 +0900

    add: page transiton

--- web/src/views/Enter.tsx
+++ web/src/views/Enter.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
-// import Image from 'next/image'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router'
 import styles from '../styles/View.module.css';
 
 const Enter: React.FC = (): JSX.Element => {
+  const router = useRouter()
+  useEffect(() => {
+    setTimeout(() => {
+      // 5seconds ago page transition
+      router.push('/home')
+    }, 5000);
+  }, [])
   return (
     <div className={styles.view}>
       
