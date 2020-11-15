5d81ed19d1437911a351ff9f7950f48f2e719d11
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-15 22:58:37 +0900

    design: MainView component

--- web/src/views/MainView/MainView.tsx
+++ web/src/views/MainView/MainView.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { 
+  IconButton,
+  Card,
+  CardContent,
+  Typography 
+
+} from '@material-ui/core'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import { Pagination } from '@material-ui/lab';
+import useStyle from './style';
 
 const MainView: React.FC = (): JSX.Element => {
+  const classes = useStyle();
   return (
-    <div>
-      <p>Hello MainView</p>
+    <div className={classes.root}>
+      <div className={classes.btn}>
+        <IconButton 
+          aria-label="add"
+          size="small"
+        >
+          <AddCircleIcon className={classes.iconSize} />
+        </IconButton>
+      </div>
+      <div>
+        <Card className={classes.card}>
+          <CardContent className={classes.cardFileTitleContent}>
+            <Typography className={classes.fileTitle}>
+              Word of the Day
+            </Typography>
+          </CardContent>
+          <CardContent className={classes.fileContent}>
+            sample.txt
+          </CardContent>
+          <CardContent>
+            <Typography className={classes.messageContent}>
+              aaaaaaaaaaaaaa
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+      <div className={classes.page}>
+        <Pagination count={10} variant="outlined" />
+      </div>
     </div>
   )
 }
