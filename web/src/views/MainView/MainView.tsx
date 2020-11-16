b7bb3a0c896e8d7e95a3032662bd747bdc5b3b01
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-16 22:54:26 +0900

    add: delete icon button

--- web/src/views/MainView/MainView.tsx
+++ web/src/views/MainView/MainView.tsx
@@ -4,9 +4,9 @@ import {
   Card,
   CardContent,
   Typography 
-
 } from '@material-ui/core'
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { Pagination } from '@material-ui/lab';
 import useStyle from './style';
 
@@ -25,9 +25,15 @@ const MainView: React.FC = (): JSX.Element => {
       <div>
         <Card className={classes.card}>
           <CardContent>
+            <Typography className={classes.dairyContent}>
+              2020/11/13
+            </Typography>
             <Typography className={classes.fileTitle}>
               Word of the Day
             </Typography>
+            <IconButton aria-label="delete" className={classes.deleteBtn}>
+              <DeleteIcon className={classes.deleteIcon} />
+            </IconButton>
           </CardContent>
         </Card>
       </div>
--- web/src/views/MainView/style.ts
+++ web/src/views/MainView/style.ts
@@ -17,9 +17,22 @@ const useStyle = makeStyles({
   card: {
     backgroundColor: "#292A2D",
     width: 350,
+    height: 75
   },
   fileTitle: {
     color: "#9e9e9e",
+  },
+  dairyContent: {
+    color: "#9e9e9e",
+    fontSize: "12px"
+  },
+  deleteBtn: {
+    right: "-47%",
+    marginTop: "-20%"
+  },
+  deleteIcon: {
+    fontSize: "28px",
+    color: "#9e9e9e"
   },
   page: {
     justifyContent: "center",
