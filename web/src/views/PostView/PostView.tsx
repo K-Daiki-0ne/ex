2a3b2480f63f94953dc4f13316aab4dfd79c9406
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-17 23:28:35 +0900

    modified: component name

--- web/src/views/PostView/PostView.tsx
+++ web/src/views/PostView/PostView.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const MainView: React.FC = (): JSX.Element => {
+const PostView: React.FC = (): JSX.Element => {
   return (
     <div></div>
   )
 }
 
-export default MainView
+export default PostView
