d5276dc923e325223795440e9911b6409251d8d4
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-23 21:45:36 +0900

    create: entry point

--- functions/app/src/main.go
+++ functions/app/src/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"EX/app/src/router"
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	app := gin.Default()
+	router.Router(app)
 	fmt.Println("APP server listening ...OK")
+	app.Run(":5050")
 }
