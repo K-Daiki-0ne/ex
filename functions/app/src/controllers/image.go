67a6fa2ac65205b0bc851a160f96226b67af58f5
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-24 22:29:35 +0900

    add: post image file

--- functions/app/src/controllers/image.go
+++ functions/app/src/controllers/image.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,14 @@ import (
 // Image : post image file
 func Image(c *gin.Context) {
 	id := c.Query("userID")
+
+	file, err := c.FormFile("image")
+
+	log.Println(file.Filename)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "Not image file")
+	}
 
 	c.JSON(http.StatusOK, id)
 }
