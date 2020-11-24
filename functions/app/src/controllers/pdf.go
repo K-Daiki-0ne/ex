97d8ee102776818f227935cbd9cec04ca290b37b
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-24 22:31:56 +0900

    add: post pdf file

--- functions/app/src/controllers/pdf.go
+++ functions/app/src/controllers/pdf.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,14 @@ import (
 // Pdf : post pdf file
 func Pdf(c *gin.Context) {
 	id := c.Query("userID")
+
+	file, err := c.FormFile("pdf")
+
+	log.Println(file.Filename)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "Not PDF file")
+	}
 
 	c.JSON(http.StatusOK, id)
 }
