2ac58b3299da2981ac0e8f28ac9680e8a27e4ac0
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-28 23:21:19 +0900

    create: TestCompareHashPassword code

--- functions/Auth/test/hash_test.go
+++ functions/Auth/test/hash_test.go
@@ -5,10 +5,10 @@ import (
 	"testing"
 )
 
-func TestHashPassword(t *testing.T) {
+// Dummy Password
+var password string = "password"
 
-	// Dummy Password
-	var password string = "password"
+func TestHashPassword(t *testing.T) {
 
 	suc, err := lib.HashPassword(password)
 
@@ -19,4 +19,20 @@ func TestHashPassword(t *testing.T) {
 	t.Log(suc)
 
 	t.Log("Test HashPassword ...OK")
+}
+
+func TestCompareHashPassword(t *testing.T) {
+	hashedPass, fail := lib.HashPassword(password)
+
+	if fail != nil {
+		t.Fatalf("Create HashPassword ...NO")
+	}
+
+	err := lib.CompareHashPassword(hashedPass, password)
+
+	if err != nil {
+		t.Fatalf("Test CompareHashPassword ...NO")
+	}
+
+	t.Log("Test CompareHashPassword ...OK")
 }
