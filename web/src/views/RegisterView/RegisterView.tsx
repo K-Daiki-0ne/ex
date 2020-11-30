fc61f3e8d3c231763a4a9036984eae65a9a09764
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-11-30 23:55:29 +0900

    add: component state & page routing event

--- web/src/views/RegisterView/RegisterView.tsx
+++ web/src/views/RegisterView/RegisterView.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import Link from 'next/link'
+import React, { FC, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import {
   Card,
   CardActions,
   CardContent,
   Button,
-  Typography,
   TextField
 } from '@material-ui/core';
 import { RegisterHeader } from '../../components/atoms'
 import useStyle from './style';
 
-const RegisterView: React.FC = (): JSX.Element => {
+const RegisterView: FC = (): JSX.Element => {
+  const [isValid, setIsValid] = useState<boolean>(false);
+  const [registerName, setRegisterName] = useState<string>("");
+  const [registerPass, setRegisterPass] = useState<string>("");
+
   const classes = useStyle();
+
+  const router = useRouter();
+
+  const registerUserInformation = () => {
+    setIsValid(false);
+
+    try {
+      setIsValid(true);
+    } catch (err) {
+      setIsValid(false);
+    }
+
+    if (isValid) {
+      router.push(`/main/${registerName}`)
+    } else {
+      // helperTextにエラーを表示する。
+    }
+  }
+
   return (
     <div className={classes.register}>
       <RegisterHeader />
@@ -30,7 +53,8 @@ const RegisterView: React.FC = (): JSX.Element => {
             inputProps={{
               className: classes.root
             }}
-
+            value={registerName}
+            onChange={e => setRegisterName(e.target.value)}
           />
           <br />
           <TextField 
@@ -43,6 +67,8 @@ const RegisterView: React.FC = (): JSX.Element => {
             inputProps={{
               className: classes.root
             }}
+            value={registerPass}
+            onChange={e => setRegisterPass(e.target.value)}
           />
         </form>
         </CardContent>
@@ -53,6 +79,7 @@ const RegisterView: React.FC = (): JSX.Element => {
             size="small"
             fullWidth
             className={classes.btn}
+            onClick={registerUserInformation}
           >
             <Link href='/main/123456' >
               <p>Register</p>
