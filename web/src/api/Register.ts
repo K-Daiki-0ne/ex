dd80d94e0754013f19b9dc24147b798e9cf70b52
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-12-01 21:35:00 +0900

    modified: Exception handling

--- web/src/api/Register.ts
+++ web/src/api/Register.ts
@@ -12,6 +12,10 @@ type Register = {
  * @returns { object }
  */
 export async function postUserInformation(name: string, password: string):Promise<Register>{
-  const data: Register = await axios.get(`http://localhost:4000/auth/login/${name}/${password}`);
-  return data;
+  try {
+    const data: Register = await axios.get(`http://localhost:4000/auth/login/${name}/${password}`);
+    return data;
+  } catch(err) {
+    return err;
+  }
 }
