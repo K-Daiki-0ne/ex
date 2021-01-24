cde27e92ba138426b351d9c92bced69c1e62d086
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-01-24 22:26:17 +0900

    modified: image file upload function

--- functions/app/src/controllers/image.go
+++ functions/app/src/controllers/image.go
@@ -1,10 +1,10 @@
 package controllers
 
 import (
-	"io"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,21 +22,41 @@ func Image(c *gin.Context) {
 	if err != nil {
 		log.Panic(err)
 		c.String(http.StatusBadRequest, "Bad request")
-		return
 	}
-	fileName := header.Filename
-	dir, _ := os.Getwd()
-	out, err := os.Create(dir + "\\images\\" + fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer out.Close()
-	_, err = io.Copy(out, file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-	})
 
+	defer file.Close()
+
+	// output filename for log
+	fmt.Printf("Uploaded File: %+v\n", header.Filename)
+
+	// output file size for log
+	fmt.Printf("File Size: %+v\n", header.Size)
+
+	fmt.Printf("MIME Header: %+v\n", header.Header)
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile("image", "upload-*.png")
+
+	if err != nil {
+
+		// output error log
+		log.Panic(err)
+	}
+
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "Successfully Uploaded File",
+	})
 }
