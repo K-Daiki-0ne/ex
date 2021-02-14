9f557dc5068d017339b1b43328cf929f1506f9c5
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-02-14 11:33:37 +0900

    create: GuestLoginText component in molecules

---
+++ web/src/components/molecules/GuestLoginText/GuestLoginText.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import Link from 'next/link';
+import { Typography } from '@material-ui/core';
+import useStyle from './style';
+
+export const GuestLoginText = () => {
+  const classes = useStyle();
+
+  return (
+    <Typography className={classes.guestLoginText}>
+      Don't have account? 
+      <Link href='/register'>
+        <a className={classes.guestLoginLinkText}>  Register</a>
+      </Link>
+    </Typography>
+  )
+}
