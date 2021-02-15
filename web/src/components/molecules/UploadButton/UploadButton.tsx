a639cdcdec2dbf9f51f852a63d5c9085869263f4
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-02-16 06:13:40 +0900

    modified: Upload Button design

--- web/src/components/molecules/UploadButton/UploadButton.tsx
+++ web/src/components/molecules/UploadButton/UploadButton.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
-import { IconButton } from '@material-ui/core';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
+import { Typography ,Button } from '@material-ui/core';
+
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import useStyle from './style';
 
 type Props = {
@@ -11,13 +12,17 @@ type Props = {
 export const UploadButton: FC<Props> = ({ props }):JSX.Element => {
   const classes = useStyle();
   return (
-    <IconButton 
-        aria-label="add"
-        size="small"
-      >
+    <Button
+      variant="contained"
+      color="primary"
+      className={classes.button}
+      startIcon={<CloudUploadIcon />}
+    >
       <Link href={`/post/${props}`}>
-        <AddCircleIcon className={classes.iconSize} />
-      </Link>
-    </IconButton>
+        <Typography>
+          Upload
+        </Typography>
+       </Link>
+    </Button>
   )
 }
--- web/src/components/molecules/UploadButton/style.ts
+++ web/src/components/molecules/UploadButton/style.ts
@@ -1,10 +1,25 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyle = makeStyles((theme) =>({
-  iconSize: {
-    fontSize: "43px",
-    color: "#9e9e9e"
-  },
+  button: {
+    margin: theme.spacing(1),
+    width: "80%",
+    backgroundColor: '#00b0ff',
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+    
+  }
 }));
 
 
