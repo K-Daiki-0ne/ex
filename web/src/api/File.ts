ed9c2f0aa117a7baa3697c96de9a27bd416b7c44
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-02-16 23:09:06 +0900

    modified: file name & class name

--- web/src/api/File.ts
+++ web/src/api/File.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-class Get {
+class File {
   public async getAllFiles(id: string) {
     const data = await axios.get(`http://localhost:5050/app/all/files?userID=${id}`);
     const response = await data.data;
@@ -8,4 +8,4 @@ class Get {
   }
 }
 
-export default new Get();
+export default new File();
