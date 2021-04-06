e55bebd3298218c484752e7762df402d674e0b63
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-04-06 22:14:36 +0900

    create: getSingleURL function

---
+++ web/src/lib/getSingleURL.ts
@@ -0,0 +1,20 @@
+/**
+ *  @description 単一のファイルを取得するAPIのエンドポイントを判定する。
+ *  @param { string } fileID    ファイルのID
+ *  @param { stirng } fileType  ファイルの種類
+ */
+export const getSingleURL = (fileID: string, fileType: string): string => {
+  switch (fileType) {
+    case 'text':
+      return `http://localhost:5050/app/single/text?fileID=${fileID}`
+      break;
+    case 'image':
+      return `http://localhost:5050/app/single/image?fileID=${fileID}`
+      break;
+    case 'pdf':
+      return `http://localhost:5050/app/single/pdf?fileID=${fileID}`
+      break;
+    default:
+      break;
+  }
+}
