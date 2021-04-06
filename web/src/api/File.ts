9e8c571b68be935004970bd131f530c006b41c2a
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-04-06 22:24:06 +0900

    create: get single file api method

--- web/src/api/File.ts
+++ web/src/api/File.ts
@@ -1,5 +1,21 @@
 import axios from 'axios';
 
+type SingleFileAPIType = {
+  data: FileAPIType
+}
+
+type FileAPIType = {
+  ID: number,
+  CreatedAt: string,
+  UpdatedAt: string,
+  DeletedAt: string,
+  userid: string,
+  FileName: string,
+  file: string,
+  Title: string,
+  Comment: string,
+}
+
 class FileAPI {
   public async getAllFiles(id: string | string[]) {
     const data = await axios.get(`http://localhost:5050/app/all/files?userID=${id}`);
@@ -16,6 +32,16 @@ class FileAPI {
       return data;
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  public async getSingleFile(url: string): Promise<FileAPIType> {
+    try {
+      const response: SingleFileAPIType = await axios.get(url);
+      const data: FileAPIType = await response.data;
+      return data;
+    } catch(err) {
+      console.error(err);
     }
   }
 }
