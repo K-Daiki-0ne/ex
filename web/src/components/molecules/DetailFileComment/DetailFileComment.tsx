bfb39ee708a7321a65f3a3b448fc7680bd7e2fd9
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-04-13 22:33:09 +0900

    modified: props name for DetailFileComment

--- web/src/components/molecules/DetailFileComment/DetailFileComment.tsx
+++ web/src/components/molecules/DetailFileComment/DetailFileComment.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
 import { Typography } from '@material-ui/core';
 
-export const DetailFileComment: FC<String> = (title: string):JSX.Element => {
+export const DetailFileComment: FC<String> = (comment: string):JSX.Element => {
   return (
     <Typography gutterBottom variant="h5" component="h2">
-      { title }
+      { comment }
     </Typography>
   )
 }
