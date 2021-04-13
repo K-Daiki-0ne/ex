cf682bcd1aef79d73e77f5e25265843484ca18fb
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-04-13 22:29:55 +0900

    create: DetailFileTitle component

---
+++ web/src/components/molecules/DetailFileTitle/DetailFileTitle.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { Typography } from '@material-ui/core';
+
+export const DetailFileTitle: FC<String> = (title: string):JSX.Element => {
+  return (
+    <Typography gutterBottom variant="h5" component="h2">
+      { title }
+    </Typography>
+  )
+}
