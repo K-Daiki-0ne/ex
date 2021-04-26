1b5fea58e60343b87577d8fa42380cc8c8f194cd
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-04-27 06:49:59 +0900

    create: detail file comment font-size

--- web/src/components/atoms/DetailFileComment/DetailFileComment.tsx
+++ web/src/components/atoms/DetailFileComment/DetailFileComment.tsx
@@ -1,16 +1,20 @@
 import React, { FC } from 'react';
 import { Typography } from '@material-ui/core';
+import useStyle from './style';
 
 type Props = {
   comment: string
 }
 
 export const DetailFileComment: FC<Props> = ({ comment }):JSX.Element => {
+  const classes = useStyle();
+
   return (
     <Typography 
       gutterBottom 
       variant="h5" 
       component="h2"
+      className={classes.detailFileComment}
     >
       { comment }
     </Typography>
