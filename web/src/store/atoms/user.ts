8ce0300685400d2219356ae3a9f5dd8a4259c892
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-05-02 00:57:33 +0900

    modified: User type in store/atoms

--- web/src/store/atoms/user.ts
+++ web/src/store/atoms/user.ts
@@ -1,12 +1,7 @@
 import { atom } from 'recoil';
 
 type User = {
-  CreatedAt: string,
-  DeletedAt: string,
   ID: string,
-  UpdatedAt: string,
-  password: string,
-  username: string,
 }
 
 export const userInfoState = atom<User>({
