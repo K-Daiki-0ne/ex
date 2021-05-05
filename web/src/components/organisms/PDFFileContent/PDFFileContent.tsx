55a9fb7af42f8af1f87a689c8269b6832c306f49
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-05-05 16:21:38 +0900

    create: division PDF file content component

---
+++ web/src/components/organisms/PDFFileContent/PDFFileContent.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+type Props = {
+  base64String: string,
+  file: string
+}
+
+export const PDFFileContent: FC<Props> = ({ base64String, file }) => {
+  return (
+    <div>
+      <Document     
+        file={`${base64String}${file}`}
+      >
+        <Page
+          width={390}
+          pageNumber={1}
+        />
+      </Document>
+    </div>
+
+  )
+}
