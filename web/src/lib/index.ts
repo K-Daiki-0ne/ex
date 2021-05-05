458805bbb8655edc7f1992c7ee1e25585d450b16
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-05-05 23:14:58 +0900

    create: libs module one together

---
+++ web/src/lib/index.ts
@@ -0,0 +1,4 @@
+export { checkFile } from './checkFileType';
+export { getSingleURL } from './getSingleURL';
+export { parseBase64String } from './parseBase64String';
+export { updateSingleURL } from './updateSingleURL';
