34774999b456bdd3d9fb9f0c2c6eaa4bd00e1f08
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-05-23 22:13:03 +0900

    create: atoms components test code

---
+++ web/src/__tests__/atoms/ButtonText.spec.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import {
+  Typography,
+  ThemeProvider
+} from '@material-ui/core'
+import theme from '../../theme/theme';
+import { ButtonText } from '../../components/atoms'
+
+let props: string;
+
+beforeEach(() => {
+  // propsのセットアップ
+  props = 'test';
+})
+
+afterEach(() => {
+  // propsの初期化
+  props = '';
+})
+
+describe('ButtonText component test', () => {
+  const wrapper = shallow (
+    <ThemeProvider theme={theme}>
+      <ButtonText text={props}/>
+    </ThemeProvider>
+  )
+
+  it('snapshot test', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('material-ui typography component rendering test', () => {
+    const typography = wrapper.find(Typography);
+    expect(typography).toBeTruthy();
+    expect(typography.length).toBe(1);
+  });
+
+  it('props test', () => {
+    expect(props).toEqual('test');
+  });
+})
